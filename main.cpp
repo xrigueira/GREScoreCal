85a5adab5de15058bb47ee208c2e3bb03818d9f9
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2022-11-17 19:39:28 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// 1. Read the data for Quant and Verb as a matrix
+// 2. Ask the user for the number of right answers in each section
+// 3. Enter each matrix with the user input (rows -> first sec | cols -> second sec)
+// 4. Calculate the global score
+// 5. Output the results (Quant score, Verb score and Global Score [Quant + Verb])
+
+int main() {
+
+    
+    return 0;
+
+}
